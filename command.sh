1b54c9c3f909505479cb1e89695d629772d67587
Parthkumar Desai <pdesai1894@gmail.com>
2017-05-21 20:01:15 -0700

    version 3

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/T8kSxgsRgYE977nwRKMkKi' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/ApkmyS2qCZoJAsPcgaJpxk' --NotebookApp.token='' --NotebookApp.allow_origin='*'
